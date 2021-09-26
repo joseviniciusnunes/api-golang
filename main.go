36e17069512f66afda4b2e17399f33369d2ac567
José Vinicius <joseviniciuus@outlook.com>
2021-09-26 02:54:44 -0300

    inicio

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/joseviniciusnunes/api-notificacao-golang/src/app/aplicativo"
 )
 
 func main() {
+
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+
 	router := mux.NewRouter()
 	aplicativo.RegisterRoute(router)
-	http.ListenAndServe(":3000", router)
+	fmt.Println("Listen on " + port)
+	http.ListenAndServe(":"+port, router)
 }
